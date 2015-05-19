471f02503e0517fa867cc28666f45e7008543acc
papajo <joshipv2@gmail.com>
2015-05-19 14:34:56 -0400

    changes to store tweets in mongoDB - 3

--- db/schemas/tweet.js
+++ db/schemas/tweet.js
@@ -1,15 +1,21 @@
 var mongoose = require('mongoose');
-var tweetSchema = mongoose.Schema;
+var Schema = mongoose.Schema;
 
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
+var tweetSchema = new Schema({
+  userId: String,
+  created: { type: Number},
+  text: String
+
+});
 tweetSchema.methods.toClient = function() {
   var tweet = 	{
-      				id: this._id,
+     				id: this._id,
      				userId: this.userId,
      				created: this.created,
      				text: this.text
-  			  	}
+  				}
   return tweet
 }
 
