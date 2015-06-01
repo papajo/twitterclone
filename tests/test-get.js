e900ca2636193a2ce0a1a41f9798bbf9f85d2789
papajo <joshipv2@gmail.com>
2015-06-01 13:47:11 -0400

    T-41

--- tests/test-get.js
+++ tests/test-get.js
@@ -1,18 +1,15 @@
 var request = require('supertest')
   , express = require('express')
 
-var app = express();
+var app = require('../index');
 
 process.env.NODE_ENV = 'test'
 
-describe('GET /api/tweets/55231d90f4d19b49441c9cb9', function() {
-	it('recieve back status code 404', function(done) {
-		request(app)
-			.get('/api/tweets/55231d90f4d19b49441c9cb9')
-			.expect(404)
-			.end(function(err, res) {
-				if(err) return done(err)
-				if(200) return done(err)
-			});
-	})
+describe('GET route', function() { 
+	it('expect to recieve server status code 404', function(done) { 
+		request(app) 
+		.get('/api/tweets/55231d90f4d19b49441c9cb9') 
+		.expect(404, done) 
+		
+	}) 
 })
