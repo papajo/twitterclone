96787b131637be73d8678ef234d8a5776e671298
papajo <joshipv2@gmail.com>
2015-06-01 14:50:00 -0400

    T-43

--- tests/test-post-tweet.js
+++ tests/test-post-tweet.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose')
-  , config = require('../config');
+  , config = require('../config')
+  , request = require('supertest')
+  , app = require('../index');
 
 process.env.NODE_ENV = 'test'
 
@@ -18,8 +20,13 @@ describe('Test suite POST /api/tweets', function() {
 		})
 		done(null);
 	})
-	it('test case scenario 1', function(done) {
-		done()
+	it('POST route 1', function(done) {
+		request(app)
+			.post('/api/tweets')
+			.set('Accept', 'text/plain')
+			.expect('Content-Type', /utf-8/)
+			.expect(403, done)
+		 
 	})
 
 	it('test case scenario 2', function(done) {
